687c600eb5e3430dc38df8c24637eef5ce724906
CAchacoso <christian_achacoso@yahoo.com>
2025-05-06 14:01:22 -0700

    update 5/6/2025
    - Changed button colors for HIRE and CONTACT
    - Added new pictures for potential SERVICES modification.

--- script.js
+++ script.js
@@ -23,15 +23,3 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
-
-$(document).ready(function(){
-    $window.scroll(function(){
-        $('.hidden').each(function(i){
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            if(bottom_of_window > bottom_of_object){
-                $(this).animate({'opacity':'1'},1500)
-            }
-        });
-    });
-});
